0ba319e14b2ca6d16d8c7476b27d8c73e0bc82e7
imran82ali <49902617+imran82ali@users.noreply.github.com>
2019-04-23 09:42:10 +0000

    Guessing game with  outer Loop
    Guessing game with  outer Loop

--- main.rs
+++ main.rs
@@ -6,35 +6,67 @@ use rand::Rng;
 
 fn main() 
 {  //Main start
+    
+    loop 
+    {//Outer Loop START
+        let mut ans_count = 0;
+        println!("\n Guess the number! \n");
+        let secret_number = rand::thread_rng().gen_range(1,101);
+        loop
+            {// Loop1 start 
+            println!("\n \n The secret number is: {}", secret_number);
 
-    println!("Guess the number!");
+            println!("\n Please input your guess.");
 
-    let secret_number = rand::thread_rng().gen_range(1,101);
-    loop
-        {// Loop1 start 
-        println!("The secret number is: {}", secret_number);
+            let mut guess = String::new();
 
-        println!("Please input your guess.");
+            io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let mut guess = String::new();
+            let guess: u32 = match guess.trim().parse(){  
+                Ok(num) => num,
+                Err(_) => continue,
+                }; 
 
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
+            println!("\n\n You guessed: {}", guess);
+            ans_count = ans_count + 1 ;
+            match guess.cmp(&secret_number) 
+            { //match start
+                Ordering::Less    => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal   => { 
+                println!("You win!"); 
+                break;}
+            } //match END
 
-        let guess: u32 = match guess.trim().parse(){  
+            if ans_count>=3
+            {  //IF Start
+            println!("\n\n Your 3 attempts are over. \n You Loose !  \n \n");
+            break;    
+            }  //IF Start
+
+        } // Loop1 END
+
+        println!("Do you want to Contnue ? \n Enetr 1 for Yes \n Enetr 2 for No \n");
+
+        let mut check = String::new();
+
+        io::stdin().read_line(&mut check).expect("Failed to read line");
+
+        let check: u32 = match check.trim().parse(){  
             Ok(num) => num,
             Err(_) => continue,
             }; 
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) 
-        { //match start
-            Ordering::Less    => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => { 
-            println!("You win!"); 
-            break;}
-        } //match END
-
-    } // Loop1 END
+        match check.cmp(&1) 
+            { //match start
+                Ordering::Less    => { 
+                println!("Thank You for your time!"); 
+                break;},
+                Ordering::Greater => { 
+                println!("Thank You for your time!"); 
+                break;},
+                Ordering::Equal   => println!("Thank you for being with us \n\n\n"), 
+            } //match END
+  
+  
+    }//Outer Loop START End
 } //Main END
